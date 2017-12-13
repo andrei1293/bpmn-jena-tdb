ba225b737eddaad18906efb5d8acc40f08d57498
Andrii Kopp <kopp93@gmail.com>
2017-12-13 19:24:50 +0200

    Update properties.

--- src/ua/khpi/properties/Properties.java
+++ src/ua/khpi/properties/Properties.java
@@ -4,6 +4,4 @@ public class Properties {
 	public static final String MODELS_PATH = "D:\\GitHub\\business-models-repository";
 
 	public static final String TRANSLATION_TOOL_PATH = "D:\\GitHub\\bpmn-xpdl-analysis-tool\\XpdlToRdfTool\\bin\\Debug\\XpdlToRdfTool.exe";
-
-	public static final String BASE_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 }
